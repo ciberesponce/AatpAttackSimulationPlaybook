a699f58c4d43b6d19a00c46ce9f962c7394d6676
Andrew Harris <aharri@microsoft.com>
2018-07-05 18:57:27 -0400

    Fixes made, plus added ability to resolve ContosoDC to dynamically update DNS settings

--- Artifacts/windows-hydrateadminpc/HydrateAdminPC.ps1
+++ Artifacts/windows-hydrateadminpc/HydrateAdminPC.ps1
@@ -1,11 +1,23 @@
 ﻿$ErrorActionPreference = "Stop"
 
-// turn on network discovery
+# Do fix for Azure DevTest Lab DNS (point to ContosoDC so we can domain join)
+# set DNS to ContosoDC IP
+# get contosoDC IP
+$contosoDcIp = (Resolve-DnsName "ContosoDC").IPAddress
+# get current DNS
+$currentDns = (Get-DnsClientServerAddress).ServerAddresses
+# add contosodc
+$currentDns += $contosoDcIp
+
+# make change to DNS with all DNS servers now
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet 2" -ServerAddresses $currentDns
+
+# turn on network discovery
 Get-NetFirewallRule -DisplayGroup 'Network Discovery'|Set-NetFirewallRule -Profile 'Private, Domain' `
     -Enabled true -PassThru|select Name,DisplayName,Enabled,Profile
 
-// Add Helpdesk to Local Admin Group
-// TODO
+# Add Helpdesk to Local Admin Group
+# TODO
 
-// Remove Domain Admins from Local Admin Group
-// TODO
+# Remove Domain Admins from Local Admin Group
+# TODO
--- Artifacts/windows-hydratevictimpc/HydrateVictimPC.ps1
+++ Artifacts/windows-hydratevictimpc/HydrateVictimPC.ps1
@@ -1,8 +1,21 @@
 ﻿$ErrorActionPreference = "Stop"
 
-// Turn on network discovery
+# Do fix for Azure DevTest Lab DNS (point to ContosoDC)
+# set DNS to ContosoDC IP
+# get contosoDC IP
+try { $contosoDcIp = (Resolve-DnsName "ContosoDC").IPAddress }
+catch{ Write-Error "Unable to find ContosoDC; make sure its in network before executing this artifact" }
+# get current DNS
+$currentDns = (Get-DnsClientServerAddress).ServerAddresses
+# add contosodc
+$currentDns += $contosoDcIp
+
+# make change to DNS with all DNS servers now
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet 2" -ServerAddresses $currentDns
+
+# Turn on network discovery
 Get-NetFirewallRule -DisplayGroup 'Network Discovery'|Set-NetFirewallRule -Profile 'Private, Domain' `
     -Enabled true -PassThru|select Name,DisplayName,Enabled,Profile
 
-// Add Helpdesk to Local Admin Group
-// TODO
+# Add Helpdesk to Local Admin Group
+# TODO
