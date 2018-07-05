59a14b41fffbcd5552050b6f1f2483575f08feda
Andrew Harris <aharri@microsoft.com>
2018-07-05 14:33:03 -0400

    Fixes made to include better verbose output

--- Artifacts/windows-ad_addRsat/Add-WindowsFeatureADRsat.ps1
+++ Artifacts/windows-ad_addRsat/Add-WindowsFeatureADRsat.ps1
@@ -1,1 +1,9 @@
-Add-WindowsFeature RSAT-AD-AdminCenter
+$ErrorActionPreference = "Stop"
+
+try {
+	Add-WindowsFeature RSAT-AD-AdminCenter
+}
+catch {
+	Write-Error "Unable to add RSAT-AD-AdminCenter Feature"
+	exit -1
+}
--- Artifacts/windows-hydrateusers/HydrateAdUsers.ps1
+++ Artifacts/windows-hydrateusers/HydrateAdUsers.ps1
@@ -1,16 +1,24 @@
-﻿# create secure strings; required for New-ADUser next
+﻿$ErrorActionPreference = "Stop"
+
+# create secure strings; required for New-ADUser next
 $nuckCSecurePass = ConvertTo-SecureString -String 'NinjaCat123' -AsPlainText -Force
 $ronHdSecurePass = ConvertTo-SecureString -String 'FightingTiger$' -AsPlainText -Force
 $jeffvSecurePass = ConvertTo-SecureString -String 'Password$fun' -AsPlainText -Force
 
-// Create NuckC, add to Domain Admins
-New-ADUser -Name NuckC -DisplayName "Nuck Chorris" -PasswordNeverExpires $true -AccountPassword $nuckCSecurePass
-Add-ADGroupMember -Identity "Domain Admins" -Members NuckC
+try{
+	// Create NuckC, add to Domain Admins
+	New-ADUser -Name NuckC -DisplayName "Nuck Chorris" -PasswordNeverExpires $true -AccountPassword $nuckCSecurePass
+	Add-ADGroupMember -Identity "Domain Admins" -Members NuckC
 
-// Create RonHD, Create Helpdesk SG, Add RonHD to Helpdesk
-New-ADUser -Name RonHD -DisplayName "Ron Helpdesk" -PasswordNeverExpires $true -AccountPassword $ronHdSecurePass
-New-ADGroup -Name Helpdesk -GroupScope Global -GroupCategory Security
-Add-ADGroupMember -Identity "Helpdesk" -Members "RonHD"
+	// Create RonHD, Create Helpdesk SG, Add RonHD to Helpdesk
+	New-ADUser -Name RonHD -DisplayName "Ron Helpdesk" -PasswordNeverExpires $true -AccountPassword $ronHdSecurePass
+	New-ADGroup -Name Helpdesk -GroupScope Global -GroupCategory Security
+	Add-ADGroupMember -Identity "Helpdesk" -Members "RonHD"
 
-// Create JeffV
-New-ADUser -Name JeffV -DisplayName "Jeff Victim" -PasswordNeverExpires $true -AccountPassword $jeffvSecurePass
+	// Create JeffV
+	New-ADUser -Name JeffV -DisplayName "Jeff Victim" -PasswordNeverExpires $true -AccountPassword $jeffvSecurePass
+}
+catch {
+	Write-Error "Unable to hydrate AD. Check to see if users (RonHD, NuckC, JeffV) exist already. Or if the Helpdesk SG exists"
+	exit -1
+}
