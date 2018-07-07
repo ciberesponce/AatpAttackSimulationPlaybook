5ba0273fa83d5dd76fd6f7e6c2139e7d105a6f48
Andrew Harris <aharri@microsoft.com>
2018-07-06 20:13:12 -0400

    Fixed issue with ATA Center

--- Artifacts/windows-hydrateAta/HydrateAtaCenter.ps1
+++ Artifacts/windows-hydrateAta/HydrateAtaCenter.ps1
@@ -14,12 +14,6 @@ Set-DnsClientServerAddress -InterfaceAlias "Ethernet 2" -ServerAddresses $curren
 # turn on network discovery
 Get-NetFirewallRule -DisplayGroup 'Network Discovery'|Set-NetFirewallRule -Profile 'Private, Domain' -Enabled true
 
-# Add Helpdesk to local admin group
-Add-LocalGroupMember -Group "Administrators" -Member "Contoso\Helpdesk"
-
-# Remove Domain Admins from local admin group
-Remove-LocalGroupMember -Group "Administrators" -Member "Domain Admins"
-
 # hide Server Manager at logon
 New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" -Force
 New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\ServerManager\Oobe -Name DoNotOpenInitialConfigurationTasksAtLogon -PropertyType DWORD -Value "0x1" -Force
