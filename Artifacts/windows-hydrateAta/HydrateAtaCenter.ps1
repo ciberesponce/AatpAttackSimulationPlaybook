9298b585a42f8345a0781893c3a4434023440960
Andrew Harris <aharri@microsoft.com>
2018-07-07 12:26:56 -0400

    Added verbosity to ATA Center

--- Artifacts/windows-hydrateAta/HydrateAtaCenter.ps1
+++ Artifacts/windows-hydrateAta/HydrateAtaCenter.ps1
@@ -1,22 +1,58 @@
 ﻿# Do fix for Azure DevTest Lab DNS (point to ContosoDC)
 # set DNS to ContosoDC IP
 # get contosoDC IP
-$contosoDcIp = (Resolve-DnsName "ContosoDC").IPAddress
+try{
+	$contosoDcIp = (Resolve-DnsName "ContosoDC").IPAddress 
 
-# get current DNS
-$currentDns = (Get-DnsClientServerAddress).ServerAddresses
-# add contosodc to DNS list
-$currentDns += $contosoDcIp
+	# get current DNS
+	$currentDns = (Get-DnsClientServerAddress).ServerAddresses
+	# add contosodc
+	$currentDns += $contosoDcIp
+	# make change to DNS with all DNS servers 
+	Set-DnsClientServerAddress -InterfaceAlias "Ethernet 2" -ServerAddresses $currentDns
+	Write-Host "Added ContosoDC to DNS"
+}
+catch {
+	Write-Error "Unable to add ContosoDC to DNS" -ErrorAction Stop
+}
 
-# make change to DNS with all DNS servers now
-Set-DnsClientServerAddress -InterfaceAlias "Ethernet 2" -ServerAddresses $currentDns
+# Turn on network discovery
+try{
+	Get-NetFirewallRule -DisplayGroup 'Network Discovery'|Set-NetFirewallRule -Profile 'Private, Domain' -Enabled true
+	Write-Host "Put VictimPC in Network Discovery Mode"
+}
+catch {
+	Write-Error "Unable to put VictimPC in Network Discovery Mode" -ErrorAction Continue
+}
 
-# turn on network discovery
-Get-NetFirewallRule -DisplayGroup 'Network Discovery'|Set-NetFirewallRule -Profile 'Private, Domain' -Enabled true
+# hide Server Manager at logon and IE Secure Mode
+try{
+	New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" -Force
+	New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\ServerManager\Oobe -Name DoNotOpenInitialConfigurationTasksAtLogon -PropertyType DWORD -Value "0x1" -Force
 
-# hide Server Manager at logon
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" -Force
-New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\ServerManager\Oobe -Name DoNotOpenInitialConfigurationTasksAtLogon -PropertyType DWORD -Value "0x1" -Force
+	# remove IE Enhanced Security
+	Set-ItemProperty -Path “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}” -Name isinstalled -Value 0
+	Set-ItemProperty -Path “HKLM:SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}” -Name isinstalled -Value 0
+	Rundll32 iesetup.dll, IEHardenLMSettings,1,True
+	Rundll32 iesetup.dll, IEHardenUser,1,True
+	Rundll32 iesetup.dll, IEHardenAdmin,1,True
+	If (Test-Path “HKCU:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”) {
+		Remove-Item -Path “HKCU:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
+	}
+	If (Test-Path “HKCU:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”) {
+		Remove-Item -Path “HKCU:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}”
+	}
+	Write-Host "Disabled Server Manager and IE Enhanced Security"
+}
+catch {
+	Write-Error "Unable to disable IE Enhanced Security or Server Manager at startup" -ErrorAction Continue
+}
 
 # audit remote SAM
-New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\Lsa -Name RestrictRemoteSamAuditOnlyMode -PropertyType DWORD -Value "0x1" -Force
+try {
+	New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa" -Name 'RestrictRemoteSamAuditOnlyMode' -PropertyType DWORD -Value "0x1" -Force
+	Write-Host "put remote SAM settings in Audit mode"
+}
+catch {
+	Write-Error "Unable to change Remote SAM settings (needed for lateral movement graph)" -ErrorAction Continue
+}
