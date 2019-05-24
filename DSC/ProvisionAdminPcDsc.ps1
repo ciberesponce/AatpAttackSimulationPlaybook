4a263ff6e52cb84e03b60033dbfbca785098e579
Andrew Harris <aharri@microsoft.com>
2019-05-24 14:35:52 -0400

    Fixed some issues with azuredeploy

--- DSC/ProvisionAdminPcDsc.ps1
+++ DSC/ProvisionAdminPcDsc.ps1
@@ -1,24 +1,19 @@
 Configuration SetupAdminPc
 {
     param(
-        # Credential to domain join
+		[Parameter(Mandatory=$true)]
+        [PSCredential]$DomainCreds,
+        
         [Parameter(Mandatory=$true)]
-		[PSCredential]$DomainCreds,
+        [string]$DomainName,
 
-        # DomainName
         [Parameter(Mandatory=$true)]
-        [String]
-        $NetBiosName,
-
-        # DNS Server in case not set by vNet
-        [Parameter(Mandatory=$true)]
-        [String]
-        $DnsServer
-    )
+        [string]$DnsServer
+	)
     Import-DscResource -ModuleName xComputerManagement, xDefender, xPSDesiredStateConfiguration, xNetworking, xStorage, xDefender, `
     PSDesiredStateConfiguration
 
-	[System.Management.Automation.PSCredential]$Creds = New-Object System.Management.Automation.PSCredential ("${NetBiosName}\$($DomainCreds.UserName)", $DomainCreds.Password)
+	[System.Management.Automation.PSCredential]$Creds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($DomainCreds.UserName)", $DomainCreds.Password)
 
     $Interface=Get-NetAdapter | Where-Object Name -Like "Ethernet*"|Select-Object -First 1
 	$InterfaceAlias=$($Interface.Name)
