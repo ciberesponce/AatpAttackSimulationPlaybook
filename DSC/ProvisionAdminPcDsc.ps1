5eca57e5201818048bcc78cdcc5dad14783b8e56
Andrew Harris <aharri@microsoft.com>
2019-05-29 16:41:46 -0400

    Fixed DNS issue with AdminPC Dsc

--- DSC/ProvisionAdminPcDsc.ps1
+++ DSC/ProvisionAdminPcDsc.ps1
@@ -12,14 +12,18 @@ Configuration SetupAdminPc
     # )
     [string]$DomainName = "Contoso.Azure"
     [string]$NetBiosName = "Contoso"
+    [string]$DnsServer = "10.0.24.4"
 
     [string]$User = "RonHD"
     $Pass = ConvertTo-SecureString "FightingTiger$" -AsPlainText -Force 
 
+    Import-DscResource -ModuleName PSDesiredStateConfiguration, xDefender, ComputerManagementDsc, xNetworking
+
+    $Interface=Get-NetAdapter | Where-Object Name -Like "Ethernet*"|Select-Object -First 1
+	$InterfaceAlias=$($Interface.Name)
+
     # $User = $AdminCred.UserName
     # $Pass = $AdminCred.Password
-
-    Import-DscResource -ModuleName PSDesiredStateConfiguration, xDefender, ComputerManagementDsc
 
 	# [PSCredential]$Creds = New-Object System.Management.Automation.PSCredential ("${NetBiosName}\$($RonHdCreds.UserName)", $RonHdCreds.Password)
 	[PSCredential]$Creds = New-Object System.Management.Automation.PSCredential ("${NetBiosName}\$User)", $Pass)
@@ -34,20 +38,29 @@ Configuration SetupAdminPc
             ActionAfterReboot = 'ContinueConfiguration'
         }
 
-        Computer JoinDomain
-        {
-            Name = 'AdminPC'
-            DomainName = $DomainName
-            Credential = $Creds            
+		xDnsServerAddress DnsServerAddress 
+		{ 
+			Address        = $DnsServer
+			InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            Validate = $true
         }
 
-        Group AddAdmins
-        {
-            GroupName = 'Administrators'
-            MembersToInclude = "Helpdesk"
-            Ensure = 'Present'
-            DependsOn = '[xComputer]JoinDomain'
-        }
+        # Computer JoinDomain
+        # {
+        #     Name = 'AdminPC'
+        #     DomainName = $DomainName
+        #     Credential = $Creds      
+        #     DependsOn = "[xDnsServerAddress]DnsServerAddress"
+        # }
+
+        # Group AddAdmins
+        # {
+        #     GroupName = 'Administrators'
+        #     MembersToInclude = "Helpdesk"
+        #     Ensure = 'Present'
+        #     DependsOn = '[Computer]JoinDomain'
+        # }
 
         xMpPreference DefenderSettings
         {
