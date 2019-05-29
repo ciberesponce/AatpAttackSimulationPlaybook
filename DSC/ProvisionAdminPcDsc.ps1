1605efe1bbc7ed87a2f7b2205b8a1da27147a6a0
Andrew Harris <aharri@microsoft.com>
2019-05-28 22:13:20 -0400

    Removed everything but AV from AdminPC

--- DSC/ProvisionAdminPcDsc.ps1
+++ DSC/ProvisionAdminPcDsc.ps1
@@ -10,19 +10,11 @@ Configuration SetupAdminPc
     #     [Parameter(Mandatory=$true)]
     #     [PSCredential] $AdminCred
     # )
-    [string]$DomainName = "Contoso.Azure"
-    [string]$NetBiosName = "Contoso"
-
-    [string]$User = "RonHD"
-    $Pass = ConvertTo-SecureString "FightingTiger$" -AsPlainText -Force 
-
-    # $User = $AdminCred.UserName
-    # $Pass = $AdminCred.Password
 
     Import-DscResource -ModuleName xDefender
 
 	# [PSCredential]$Creds = New-Object System.Management.Automation.PSCredential ("${NetBiosName}\$($RonHdCreds.UserName)", $RonHdCreds.Password)
-	[PSCredential]$Creds = New-Object System.Management.Automation.PSCredential ("${NetBiosName}\$User)", $Pass)
+	# [PSCredential]$Creds = New-Object System.Management.Automation.PSCredential ("${NetBiosName}\$User)", $Pass)
 
     Node localhost
     {
