6ce17bc191a218db3705e88ef0f9eb77485b436e
Andrew Harris (AZURE SEC) <aharri@microsoft.com>
2019-06-05 14:14:22 -0400

    Ensure all Creds use NetBiosName\UserName

--- DSC/ProvisionAdminPcDsc.ps1
+++ DSC/ProvisionAdminPcDsc.ps1
@@ -27,9 +27,8 @@ Configuration SetupAdminPc
     $InterfaceAlias=$($Interface.Name)
 
     $SamiraASmbScriptLocation = [string]'C:\ScheduledTasks\SamiraASmbSimulation.ps1'
-    # [PSCredential]$SamiraADomainCred = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($SamiraACred.UserName)", $SamiraACred.Password)
+    [PSCredential]$SamiraADomainCred = New-Object System.Management.Automation.PSCredential ("${NetBiosName}\$($SamiraACred.UserName)", $SamiraACred.Password)
 	[PSCredential]$Creds = New-Object System.Management.Automation.PSCredential ("${NetBiosName}\$($AdminCred.UserName)", $AdminCred.Password)
-	# [PSCredential]$Creds = New-Object System.Management.Automation.PSCredential ("${NetBiosName}\$User)", $Pass)
 
     Node localhost
     {
