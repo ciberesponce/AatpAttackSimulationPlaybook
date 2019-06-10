a5f1347a89e5faf63a6eb4ad8e1ec0c081362776
Andrew Harris (AZURE SEC) <aharri@microsoft.com>
2019-06-10 16:30:23 -0400

    Updates and republshing DSCs

--- Troubleshoot/Troubleshoot-Helpers.ps1
+++ Troubleshoot/Troubleshoot-Helpers.ps1
@@ -1,17 +1,15 @@
-function Remove-Extensions {
-    param(
-        # ResourceGroupName
-        [Parameter(Mandatory=$false)]
-        [string]
-        $resourceGroup='Andrew-Test'
-    )
-    $vmData = @(
+param(
+    # ResourceGroupName
+    [Parameter(Mandatory = $false)]
+    [string]
+    $resourceGroup = 'Andrew-Test'
+)
+$vmData = @(
     ('Client01', 'AipDsc'),
     ('AdminPc', 'AdminPcDsc'),
     ('ContosoDc', 'DcPromoDsc'),
     ('VictimPc', 'VictimDsc')
 )
-    foreach ($vmSet in $vmData){
-        Remove-AzVMExtension -ResourceGroupName $resourceGroup -VMName $vmset[0] -Name $vmSet[1] -Force
-    }
+foreach ($vmSet in $vmData) {
+    Remove-AzVMExtension -ResourceGroupName $resourceGroup -VMName $vmset[0] -Name $vmSet[1] -Force
 }
